c2bfd41275026f0d1533db392b6abe6742012dd4
Thiago <thiago.goulart001@edu.pucrs.br>
2024-01-28 02:55:14 -0300

    Adding new login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-import styles from "./App.module.css"
+import styles from "./App.module.css";
+import { Login } from "./pages/login/Login";
 
 function App() {
   return (
     <div className={styles.App}>
+      <Login />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
