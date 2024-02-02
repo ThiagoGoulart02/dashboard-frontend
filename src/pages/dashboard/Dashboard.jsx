2f1e0162529d84f484a7595c15c17e833f1c6003
Thiago <thiago.goulart001@edu.pucrs.br>
2024-02-02 01:52:35 -0300

    Creating the container of the dashboard page

---
+++ src/pages/dashboard/Dashboard.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Container } from "../../components/container/Container";
+
+const Dashboard = () => {
+  return <Container></Container>;
+};
+
+export default Dashboard;
