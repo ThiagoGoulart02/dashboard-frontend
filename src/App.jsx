5e1d637962e296deab8677e2b85fb69f617fe229
Thiago Goulart <thiago.goulart001@edu.pucrs.br>
2024-02-18 03:54:19 -0300

    Fixing the package-lock

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import styles from "./App.module.css";
 import { Login } from "./pages/login/Login";
+import Dashboard from "./pages/dashboard/Dashboard"
 
 function App() {
   return (
     <div className={styles.App}>
-      <Login />
+      <Dashboard />      
     </div>
   );
 }
